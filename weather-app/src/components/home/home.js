02b8fb5bd3fd81ecdb34ab99d27f88ee2bd63cc8
xaviertanwc <66908992+xaviertanwc@users.noreply.github.com>
2023-12-15 13:10:00 +0800

    ✨ Finish SearchResult functionality

--- weather-app/src/components/home/home.js
+++ weather-app/src/components/home/home.js
@@ -1,12 +1,8 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import '../../App.css';
 import './home.css'
-import { useForm } from 'react-hook-form';
-import { Button, Input, Form, List, Select, AutoComplete } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faMultiply } from '@fortawesome/free-solid-svg-icons'
-import { getAllCountries, getWeatherByLatLon, getCityByPrefixAndCountry, getWeatherByCityAndCountry } from '../request/request';
 import SearchBar from '../search-bar/search-bar';
+import { format } from 'date-fns';
 
 const MyContext = createContext();
 
@@ -16,7 +12,8 @@ function AppHome() {
 
   const handleWeather = (response) => {
     if (response.cod === 200) {
-      setWeather(response);
+      setWeather({...response, dateTime: format(new Date(), 'dd-MM-yyyy hh:mm a')});
+      console.log(weather);
       setErrorRequest('')
     } else {
       setErrorRequest(response.message);
@@ -32,22 +29,25 @@ function AppHome() {
             <img src={require('../../assets/images/cloud.png')} alt="fireSpot"/>
             <div className='CurrentSearchContainer'>
               <div className='full-column'>Today's Weather</div>
-              <div className='full-column'>{Math.round(parseFloat(weather?.main?.temp))}</div>
+              <div className='full-column MainTemp'>{Math.round(parseFloat(weather?.main?.temp))}&#176;</div>
               <div className='full-column'>
-                <div>{Math.round(parseFloat(weather?.main?.temp_max))}</div>
-                <div>{Math.round(parseFloat(weather?.main?.temp_min))}</div>
+                H: {Math.round(parseFloat(weather?.main?.temp_max))}&#176;
+                L: {Math.round(parseFloat(weather?.main?.temp_min))}&#176;
               </div>
-              <div>{weather?.name}</div>
-              <div>{weather?.sys?.country}</div>
-              <div>{weather?.main?.humidity}</div>
-              <div>{weather?.weather?.length? weather?.weather[0].main : ''}</div>
+              <div className='CityCountryText'>{weather?.name}, {weather?.sys?.country}</div>
+              <div className='OtherWeatherDetails'>{weather?.dateTime?.toString()}</div>
+              <div className='OtherWeatherDetails'>Humidity: {weather?.main?.humidity}%</div>
+              <div className='OtherWeatherDetails'>{weather?.weather?.length? weather?.weather[0].main : ''}</div>
             </div>
           </div>
         ) : (
           <div></div>
         ) : (
-          <div>{errorRequest}</div>
+          <div className='ErrorText'>{errorRequest}</div>
         )}
+      </div>
+      <div className='SearchHistory'>
+
       </div>
     </div>
   );
